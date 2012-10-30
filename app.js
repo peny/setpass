0599c8bf804d0932a018a678fc5ff1a33f67508c
petter <petter@pttr.se>
2012-10-30 16:14:33 +0100

    login works for facebook, change does not

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var Browser = require('zombie');
 
 var facebook = {
-    changelink : 'https://www.facebook.com/settings?tab=account&section=password&view'
+    loginlink       : 'https://www.facebook.com',
+    loginfield      : '#email',
+    passwordfield   : '#pass',
+    signinfield     : '#loginbutton > input',
+    changelink      : 'https://www.facebook.com/settings?tab=account&section=password&view',
+    change_current  : 'password_old',
+    change_new      : 'password_new',
+    change_confirm  : 'password_confirm',
+    change_save     : '.submit > input'
 };
 
 var twitter = {
@@ -16,31 +24,47 @@ var twitter = {
     change_save     : 'Save changes'
 };
 
+var default_service = {
+    loginlink       : '',
+    loginfield      : '',
+    passwordfield   : '',
+    signinfield     : '',
+    changelink      : '',
+    change_current  : '',
+    change_new      : '',
+    change_confirm  : '',
+    change_save     : ''
+};
+
 var user = {
     username    : '',
     password    : '',
     newpassword : ''
 };
 
-var service = twitter;
+var service = facebook;
 
 var browser = new Browser();
 browser.visit(service.loginlink,function(){
+    console.log(browser.text("title"));
     browser.
         fill(service.loginfield, user.username). 
         fill(service.passwordfield, user.password).
         pressButton(service.signinfield, function(){
             browser.visit(service.changelink,function(){
                 console.log(browser.location.pathname);
-                console.log(browser.text("body"));
+                console.log(browser.text("title"));
                 browser.
                     fill(service.change_current, user.password).
                     fill(service.change_new, user.newpassword).
-                    fill(service.change_confirm, user.newpassword).
-                    pressButton(service.change_save,function(){
-                        console.log(browser.location.pathname);
-                        console.log(browser.text("body"));
+                    fill(service.change_confirm, user.newpassword);
+                    browser.fire('click', service.change_confirm,function(){
+                        browser.pressButton(service.change_save,function(){
+                            console.log(browser.location.pathname);
+                            console.log(browser.text("title"));
+                         });
                     });
+
             });
         });
 });
