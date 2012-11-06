0142d7df71b7d3ac946057dad5ab875d91593394
petter <petter@pttr.se>
2012-11-06 14:14:17 +0100

    fully working for facebook

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var Browser = require('zombie');
 
 var facebook = {
+    name            : 'facebook',
     loginlink       : 'https://www.facebook.com',
     loginfield      : '#email',
     passwordfield   : '#pass',
@@ -13,6 +14,7 @@ var facebook = {
 };
 
 var twitter = {
+    name            : 'twitter',
     loginlink       : 'https://twitter.com/login/?lang=en',
     loginfield      : 'Username or email',
     passwordfield   : 'Password',
@@ -51,20 +53,20 @@ browser.visit(service.loginlink,function(){
         fill(service.loginfield, user.username). 
         fill(service.passwordfield, user.password).
         pressButton(service.signinfield, function(){
-            browser.visit(service.changelink,function(){
+        browser.visit(service.changelink,function(){
+            console.log(browser.location.pathname);
+            console.log(browser.text("title"));
+            //For facebook
+            browser.evaluate('document.querySelector("'+service.change_save+'").removeAttribute("disabled");');
+            browser.
+                fill(service.change_current, user.password).
+                fill(service.change_new, user.newpassword).
+                fill(service.change_confirm, user.newpassword);
+                browser.pressButton(service.change_save,function(){
                 console.log(browser.location.pathname);
                 console.log(browser.text("title"));
-                browser.
-                    fill(service.change_current, user.password).
-                    fill(service.change_new, user.newpassword).
-                    fill(service.change_confirm, user.newpassword);
-                    browser.fire('click', service.change_confirm,function(){
-                        browser.pressButton(service.change_save,function(){
-                            console.log(browser.location.pathname);
-                            console.log(browser.text("title"));
-                         });
-                    });
-
             });
+
         });
+    });
 });
